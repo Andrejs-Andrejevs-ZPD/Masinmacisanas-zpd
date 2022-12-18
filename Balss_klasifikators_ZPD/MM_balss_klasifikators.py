f942f4a80d6588b874e02c926464d97392ffcae0
Andrejs-Andrejevs-ZPD <120796324+Andrejs-Andrejevs-ZPD@users.noreply.github.com>
2022-12-18 16:38:03 +0200

    Add files via upload

---
+++ Balss_klasifikators_ZPD/MM_balss_klasifikators.py
@@ -0,0 +1,292 @@
+import librosa
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import librosa.display
+import keras
+import datetime
+import seaborn as sns
+#from sklearn.preprocessing import LabelEncoder 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import mutual_info_classif as mi
+import os
+
+from sklearn import preprocessing
+
+plt.style.use('ggplot')
+plt.rcParams['font.family'] = 'sans-serif' 
+plt.rcParams['font.serif'] = 'Ubuntu' 
+plt.rcParams['font.monospace'] = 'Ubuntu Mono' 
+plt.rcParams['font.size'] = 14 
+plt.rcParams['axes.labelsize'] = 12 
+plt.rcParams['axes.labelweight'] = 'bold' 
+plt.rcParams['axes.titlesize'] = 12 
+plt.rcParams['xtick.labelsize'] = 12 
+plt.rcParams['ytick.labelsize'] = 12 
+plt.rcParams['legend.fontsize'] = 12 
+plt.rcParams['figure.titlesize'] = 12 
+plt.rcParams['image.cmap'] = 'jet' 
+plt.rcParams['image.interpolation'] = 'none' 
+plt.rcParams['figure.figsize'] = (10, 10
+                                 ) 
+plt.rcParams['axes.grid']=False
+plt.rcParams['lines.linewidth'] = 2 
+plt.rcParams['lines.markersize'] = 8
+colors = ['xkcd:pale orange', 'xkcd:sea blue', 'xkcd:pale red', 'xkcd:sage green', 'xkcd:terra cotta', 'xkcd:dull purple', 'xkcd:teal', 'xkcd: goldenrod', 'xkcd:cadet blue',
+'xkcd:scarlet']
+bbox_props = dict(boxstyle="round,pad=0.3", fc=colors[0], alpha=.5)
+
+
+trimmed = os.path.join('C:\machine_learning\Voice_debate', 'zpd_1.wav')
+y,sr = librosa.load(trimmed)
+dur_time = librosa.get_duration(y=y, sr=sr)
+time_sec = round(dur_time)
+
+S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=20, fmax=8000)
+fig, ax = plt.subplots()
+S_dB = librosa.power_to_db(S, ref=np.max)
+img = librosa.display.specshow(S_dB, x_axis='time',y_axis='mel', sr=sr,fmax=8000, ax=ax)
+
+fig.colorbar(img, ax=ax, format='%+2.0f dB')
+ax.set(title='Mel frekvences spektogramma')
+#   plt.xlim(60,120)
+plt.show()
+
+start = 0 
+end = time_sec 
+time_series_sec = np.linspace(start,end,len(S_dB[0]))
+
+#   read CSV file
+data_path = os.path.join('C:\machine_learning\Voice_debate', 'zpd_1.csv')
+data = pd.read_csv(data_path)
+
+#   Seconds converter
+def convert_to_sec(time_string):
+    time_s_list = time_string.split(':')
+    if len(time_s_list)>2:
+        date_time = datetime.datetime.strptime(time_string, "%H:%M:%S")
+        a_timedelta = date_time - datetime.datetime(1900, 1, 1)
+        seconds = a_timedelta.total_seconds()
+    else:
+        date_time = datetime.datetime.strptime(time_string, "%M:%S")
+        a_timedelta = date_time - datetime.datetime(1900, 1, 1)
+        seconds = a_timedelta.total_seconds()
+    return seconds
+
+minute_list = data.minute.tolist()
+
+SEC_LIST = []
+for t in minute_list:
+    try:
+        SEC_LIST.append(convert_to_sec(t))
+    except:
+        SEC_LIST.append(0)
+for k in range(2):
+    for i in range(len(SEC_LIST)-1):
+        if SEC_LIST[i]==SEC_LIST[i+1]:
+            SEC_LIST[i+1]=SEC_LIST[i]+1
+turn_index = SEC_LIST.index(0)
+to_add = SEC_LIST[turn_index-1]
+for j in range(turn_index,len(SEC_LIST)):
+    SEC_LIST[j]=SEC_LIST[j]+to_add
+data['Seconds']=SEC_LIST
+
+data.head()
+
+new_data = data[data.Seconds<=time_series_sec.max()]
+
+max_sec = new_data.Seconds.max()
+new_data.drop_duplicates(subset='speaker')
+
+sns.countplot(x= new_data.speaker, palette='plasma')
+plt.grid(True)
+plt.show()
+
+audio_data = pd.DataFrame(S_dB)
+audio_data = audio_data.T
+audio_data.tail()
+
+pca = PCA(n_components=4)
+pca.fit(audio_data)
+pca_data = pd.DataFrame(pca.transform(audio_data))
+
+audio_time = np.array(pca_data.index.tolist())*max_sec/np.array(pca_data.index.tolist()).max()
+nd_time_list = [0]+new_data.Seconds.tolist()
+CLASS = []
+for j in range(len(audio_time)):
+    time_j = audio_time[j]
+    for i in range(1,len(nd_time_list)):
+        start_i = nd_time_list[i-1]
+        end_i = nd_time_list[i]
+        if time_j>start_i and time_j<end_i:
+            CLASS.append(new_data.loc[i-1].speaker)
+
+pca_data['speaker']=CLASS[:len(pca_data)]
+pca_data['Time']=audio_time
+pca_data.head()
+
+#plt.grid(True)
+sns.pairplot(data =pca_data, hue='speaker', plot_kws={'s':1}, palette='plasma')
+plt.show()
+
+mi(X=pca_data[['Time',0,1,2,3]],y=pca_data['speaker'])
+time_series_data = pca_data[['Time',2,'speaker']]  
+
+#sns.scatterplot(x='Time', y='X', hue='speaker', data=time_series_data, palette='plasma')  #    check workability
+#plt.grid(True)
+#plt.show()
+
+#   Label encoding
+label_encoder = preprocessing.LabelEncoder()
+time_series_data['speaker']= label_encoder.fit_transform(time_series_data['speaker'])
+
+encod_dic = {0:'Autors',2:'Runātājs 3',1:'Runātājs 2'}
+
+
+
+
+#   DEEP LEARNING   
+
+X=time_series_data[['Time',2]]
+y=time_series_data.speaker
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+x_train = np.array(X_train)
+x_test = np.array(X_test)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
+x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+
+num_classes = len(np.unique(y_train))
+
+idx = np.random.permutation(len(x_train))
+x_train = x_train[idx]
+y_train = y_train[idx]
+
+def make_model(input_shape):
+    input_layer = keras.layers.Input(input_shape)
+    conv1 = keras.layers.Conv1D(filters=64, kernel_size=3, padding="same")(input_layer)
+    conv1 = keras.layers.BatchNormalization()(conv1)
+    conv1 = keras.layers.ReLU()(conv1)
+    conv2 = keras.layers.Conv1D(filters=64, kernel_size=3, padding="same")(conv1)
+    conv2 = keras.layers.BatchNormalization()(conv2)
+    conv2 = keras.layers.ReLU()(conv2)
+    conv3 = keras.layers.Conv1D(filters=64, kernel_size=3, padding="same")(conv2)
+    conv3 = keras.layers.BatchNormalization()(conv3)
+    conv3 = keras.layers.ReLU()(conv3)
+    gap = keras.layers.GlobalAveragePooling1D()(conv3)
+    output_layer = keras.layers.Dense(num_classes, activation="softmax")(gap)
+    return keras.models.Model(inputs=input_layer, outputs=output_layer)
+
+model = make_model(input_shape=x_train.shape[1:])
+#keras.utils.plot_model(model, show_shapes=True)
+
+epochs = 30
+batch_size = 50
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        "best_model.h5", save_best_only=True, monitor="val_loss"
+    ),
+    keras.callbacks.ReduceLROnPlateau(
+        monitor="val_loss", factor=0.5, patience=20, min_lr=0.0001
+    ),
+    keras.callbacks.EarlyStopping(monitor="val_loss", patience=50, verbose=1),
+]
+model.compile(
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["sparse_categorical_accuracy"],
+)
+history = model.fit(
+    x_train,
+    y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    callbacks=callbacks,
+    validation_split=0.2,
+    verbose=1,
+)
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+
+print("Test accuracy", test_acc)
+print("Test loss", test_loss)
+
+pred_test = pd.DataFrame(model.predict(x_test))
+pred_test = pred_test.rename(columns={0:encod_dic[0]})
+pred_test = pred_test.rename(columns={1:encod_dic[1]})
+pred_test = pred_test.rename(columns={2:encod_dic[2]})
+pred_test.head()
+
+test_data = X_test.reset_index().drop('index',axis=1)
+test_data['target']=y_test
+test_data['Autors']=pred_test['Autors']
+test_data['Runātājs 2']=pred_test['Runātājs 2']
+test_data['Runātājs 3']=pred_test['Runātājs 3']
+
+target_list = test_data.target.tolist()
+for t in range(len(target_list)):
+    target_list[t]=encod_dic[target_list[t]]
+
+test_data['target']=target_list
+test_data = test_data.sort_values(by='Time')
+print(test_data)
+"""
+plt.figure(figsize=(20,12))
+plt.subplot(2,2,1)
+plt.title('Test Set Data')
+sns.scatterplot(test_data.Time,test_data,hue=test_data.target,palette='plasma')
+plt.grid(True)
+plt.show()
+
+plt.subplot(2,2,2)
+plt.title('Chris Wallace Probability')
+sns.scatterplot(test_data.Time,test_data.X,s=10,hue=test_data['Chris Wallace'])
+plt.grid(True)
+
+plt.subplot(2,2,3)
+plt.title('Trump Probability')
+sns.scatterplot(test_data.Time,test_data.X,s=10,hue=test_data['President Donald J. Trump'])
+plt.grid(True)
+
+plt.subplot(2,2,4)
+plt.title('Biden Probability')
+sns.scatterplot(test_data.Time,test_data.X,s=10,hue=test_data['Vice President Joe Biden'])
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+"""
+def model_to_clf(input_data):
+    pred_list = model.predict(input_data)
+    LIST_PRED = []
+    for p in pred_list:
+        LIST_PRED.append(np.argmax(p))
+    return LIST_PRED
+
+cm=confusion_matrix(model_to_clf(x_test),y_test)
+cm_data = pd.DataFrame(cm,columns=[encod_dic[0],encod_dic[1],encod_dic[2]])
+cm_data.index = [encod_dic[0],encod_dic[1],encod_dic[2]]
+cm_data
+print(cm)
+
+sns.heatmap(cm_data.astype(int), fmt='d', annot=True,cmap='plasma')
+plt.yticks(rotation=0)
+plt.xticks(rotation=0,fontsize=10)
+plt.show()
+
+clf_report = classification_report(y_test,
+                                   model_to_clf(x_test),
+                                   labels= [0,1,2],
+                                   target_names= [encod_dic[0],encod_dic[1],encod_dic[2]],
+                                   output_dict=True)
+
+sns.heatmap(pd.DataFrame(clf_report).iloc[:-1, :].T, annot=True,cmap='plasma')
+plt.show()
